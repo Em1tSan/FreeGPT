6b083464f443cb7e15104bfb0a936baab50c9582
Roman S <next.attack@gmail.com>
2023-07-01 20:48:19 +0800

    Экспериментальный возврат GPT-4

--- g4f/models.py
+++ g4f/models.py
@@ -26,7 +26,7 @@ class Model:
     class gpt_35_turbo_16k:
         name: str = 'gpt-3.5-turbo-16k'
         base_provider: str = 'openai'
-        best_provider: Provider.Provider = Provider.Gravityengine
+        best_provider: Provider.Provider = Provider.Liaobots
 
     class gpt_4_dev:
         name: str = 'gpt-4-for-dev'
@@ -36,8 +36,8 @@ class Model:
     class gpt_4:
         name: str = 'gpt-4'
         base_provider: str = 'openai'
-        best_provider: Provider.Provider = Provider.Lockchat
-        best_providers: list = [Provider.Bing, Provider.Lockchat]
+        best_provider: Provider.Provider = Provider.Liaobots
+        best_providers: list = [Provider.Liaobots, Provider.Lockchat]
     
     class gpt_4_0613:
         name: str = 'gpt-4-0613'
